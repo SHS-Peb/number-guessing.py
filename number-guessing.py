e445fe37055d1fd44392d50826b3c9b77c717405
unknown <blackskiesdancer@gmail.com>
2025-04-19 00:08:30 +1000

    Basic attempt

--- number-guessing.py
+++ number-guessing.py
@@ -1,1 +1,16 @@
 import random
+
+num = 0
+comChoice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+com = random.choice(comChoice)
+
+
+def game ():
+    num = int(input("Guess a number between 1 and 10:"))
+    print (com)
+    if (num == com):
+        print("You win!")
+    else:
+        print("aw shucks you got it wrong")
+
+game()
